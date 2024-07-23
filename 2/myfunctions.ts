1487662eb68d7d816d581706364751125a921a7b
Vedantjn <vedant.jain.indore@gmail.com>
2024-07-23 18:24:06 +0530

    better way to write function

--- 2/myfunctions.ts
+++ 2/myfunctions.ts
@@ -1,10 +1,10 @@
-function addTwo(num: number){
-    // return num+2;
-    return "hi";
+function addTwo(num: number): number{
+    return num+2;
+    // return "hi"
 }
 
 function getUpper(val: string){
-    return val.toUpperCase();
+    return val.toUpperCase()
 }
 
 function signUpUser(name: string, email: string, isPaid: boolean){}
@@ -15,5 +15,39 @@ addTwo(5)
 getUpper("4")
 signUpUser("vedant", "vedant@gmail.com", true)
 loginUser("vedant", "vedant@gmail.com") 
+
+// function getValue(myVal: number): boolean{
+//     if(myVal > 5){
+//         return true
+//     }
+//     return "200 OK"
+// }
+
+const getHello = (s: string): string => {
+    return ""
+}
+
+const heroes = ["thor", "spiderman", "ironman"]
+// const heroes = [1, 2, 3]
+
+heroes.map((hero): string => {
+    return  `hero is ${hero}`
+    // return 1;
+})
+
+function consoleError(errormsg: string): void{
+    console.log(errormsg);
+    // return 1;
+}
+
+function handleError(errormsg: string): never{
+    // console.log(errormsg);
+    // return 1;
+    throw new Error(errormsg);
+}
+
+
+
+
 
 export {}
