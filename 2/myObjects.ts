641d58d01e0690c7504a345c146c321eae0397fd
Vedantjn <vedant.jain.indore@gmail.com>
2024-07-24 15:56:31 +0530

    types aliases

--- 2/myObjects.ts
+++ 2/myObjects.ts
@@ -1,22 +1,30 @@
-const User = {
-    name: "Vedant",
-    email: "vedant@gmail.com",
-    isActive: true
+// const User = {
+//     name: "Vedant",
+//     email: "vedant@gmail.com",
+//     isActive: true
+// }
+
+// function createUser({name: string, isPaid: boolean}) {}
+
+// let newUser = {name: "vedant", isPaid: false, email: "h@h.com"}
+
+// createUser(newUser)
+
+// function createCourse():{name: string, price: number}{
+//     return {name:"reactJS", price: 399}
+// }
+
+type User = {
+    name: string;
+    email: string;
+    isActive: boolean
 }
 
-function createUser({name: string, isPaid: boolean}) {}
-
-let newUser = {name: "vedant", isPaid: false, email: "h@h.com"}
-
-createUser(newUser)
-
-
-
-function createCourse():{name: string, price: number}{
-    return {name:"reactJS", price: 399}
+function createUser(user: User): User{
+    return {name: "", email: "", isActive: true}
 }
 
-
+createUser({name: "", email: "", isActive: true})
 
 
 
