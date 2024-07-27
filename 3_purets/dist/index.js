4aa34638cbd8e763a2b451b216710232c8cdaba7
Vedantjn <vedant.jain.indore@gmail.com>
2024-07-27 09:40:20 +0530

    public private

--- 3_purets/dist/index.js
+++ 3_purets/dist/index.js
@@ -1,10 +1,31 @@
 "use strict";
+// class User {
+//     public email: string
+//     // #name: string
+//     private name: string
+//     private readonly city: string = "Indore"
+//     constructor(email: string, name: string){
+//         this.email = email;
+//         this.name = name;
+//     }
+// }
+// class User {
+//     private readonly city: string = "Indore"
+//     constructor(public email: string, public name: string){
+//         this.email = email;
+//         this.name = name;
+//     }   
+// }
 class User {
     constructor(email, name) {
-        this.city = "";
+        this.email = email;
+        this.name = name;
+        this.city = "Indore";
         this.email = email;
         this.name = name;
     }
 }
 const vedant = new User("vedant@gmail.com", "vedantjn");
-vedant.city = "Indore";
+// vedant.city = "Indore"
+vedant.email;
+// vedant.city
--- 3_purets/src/index.ts
+++ 3_purets/src/index.ts
@@ -1,12 +1,38 @@
+// class User {
+//     public email: string
+//     // #name: string
+//     private name: string
+//     private readonly city: string = "Indore"
+//     constructor(email: string, name: string){
+//         this.email = email;
+//         this.name = name;
+
+//     }
+// }
+
+
+// class User {
+//     private readonly city: string = "Indore"
+//     constructor(public email: string, public name: string){
+//         this.email = email;
+//         this.name = name;
+
+//     }   
+// }
+
+
 class User {
-    email: string
-    name: string
-    city: string = ""
-    constructor(email: string, name: string){
+    private readonly city: string = "Indore"
+    constructor(
+        public email: string, 
+        public name: string
+    ){
         this.email = email;
         this.name = name;
-    }
+    }   
 }
 
 const vedant = new User("vedant@gmail.com", "vedantjn")
-vedant.city = "Indore"
+// vedant.city = "Indore"
+vedant.email
+// vedant.city
