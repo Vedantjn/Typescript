fc2f86781597e69e1f52894eb34bc45637e49cb6
Vedantjn <vedant.jain.indore@gmail.com>
2024-07-28 10:44:42 +0530

    Generics

---
+++ 3_purets/src/myGenerics.ts
@@ -0,0 +1,30 @@
+const score: Array<number> = []
+const names: Array<string> = []
+
+function identityOne(val: boolean | number): boolean | number {
+    return val
+}
+
+function identityTwo(val: any): any {
+    return val
+}
+
+function identityThree<Type>(val: Type): Type {
+    return val
+}
+
+identityThree(3);
+identityThree("3");
+identityThree("Vedant");
+identityThree(true);
+
+function identityFour<T>(val: T): T {
+    return val
+}
+
+interface Bottle{
+    brand: string,
+    type: number
+}
+
+identityFour<Bottle>({})
