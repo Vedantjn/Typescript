6b20990e8ce55be060957cf45947da3c4a51ffce
Vedantjn <vedant.jain.indore@gmail.com>
2024-07-29 13:55:34 +0530

    instanceof and type predicate

--- 3_purets/src/detection.ts
+++ 3_purets/src/detection.ts
@@ -44,6 +44,37 @@ interface Admin {
 function isAdminAccount(account: User | Admin) {
     if("isAdmin" in account) {
         return account.isAdmin
+    }   
+}
+
+// Instance of
+
+function logValue(x: Date | string) {
+    if(x instanceof Date) {
+        console.log(x.toUTCString());
     }
-    
+    else {
+        console.log(x.toUpperCase());
+        
+    }
+}
+
+// Type predicate
+
+type Fish = {swim: () => void};
+type Bird = {fly: () => void};
+
+function isFish(pet: Fish | Bird): pet is Fish {
+    return (pet as Fish).swim !== undefined
+}
+
+function getFood(pet: Fish | Bird) {
+    if(isFish(pet)) {
+        pet
+        return "Fish food"
+    }
+    else{
+        pet
+        return "Bird food"
+    }
 }
